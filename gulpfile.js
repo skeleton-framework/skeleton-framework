3619f3b54740ee389a5e68886b26a74d17027213
Gary DeLaney <gld1982ltd@users.noreply.github.com>
2015-12-13 09:34:18 -0600

    update ignores, build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,52 @@
+// dependencies
 var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     postcss = require('gulp-postcss'),
     cssmin = require('gulp-cssmin'),
     rename = require('gulp-rename'),
-    gConn = require("gulp-connect"),
+    connect = require("gulp-connect"),
     bump = require('gulp-bump'),
     header = require('gulp-header'),
     moment = require('moment'),
     pkg = require('./package.json')
 
+// define header
 var banner = ['/*!',
   ' Skeleton Framework',
   ' | <%= pkg.version %>',
   ' | <%= pkg.license %>',
-  ' | '+ moment().format("MMM Do, YYYY"),
+  ' | ' + moment().format("MMM Do, YYYY"),
   ' */',
-  '\n \n'
-].join('')
+  '\n\n'
+  ].join('')
 
+// define paths
 var paths = {
-  css: {
-    src: './src/skeleton.css',
-    dist: './dist',
-    dev: './dev/css'
+  src: {
+    css: 'src/skeleton.css',
+    html: 'src/index.html',
+    //js: 'src/skeleton.js',
+    images: 'src/images'
   },
-  html: {
-    src: './src/test.html',
-    dev: './dev/'
+  dev: {
+    css: 'dev/css',
+    html: 'dev',
+    //js: 'dev/js',
+    images: 'dev/images'
   },
-  images: {
-    src: './src/images/favicon.png',
-    dev: './dev/images'
+  dist: {
+    css: 'dist/css',
+    //html: 'dist',
+    //js: 'dist/js',
+    //images: 'dist/images'
   },
+  watch: ["src/**/*", "dev/**/*"],
   serve: {
     root: 'dev'
-  },
-  watch: ['src/**/*', 'dev/**/*']
+  }
 }
 
+// postcss plugins
 var processors = [
   require('postcss-import')(),
   require('postcss-custom-properties')(),
@@ -47,19 +56,8 @@ var processors = [
   require('autoprefixer-core')()
 ]
 
-
-
-function release(options) {
-  return gulp.src('./package.json')
-    .pipe(bump({type: options}))
-    .pipe(gulp.dest('./'))
-}
-
-gulp.task('patch', function() { return release('patch'); })
-gulp.task('feature', function() { return release('minor'); })
-gulp.task('release', function() { return release('major'); })
-
-var buildTask = function(options) {
+// css build options
+var css = function (options) {
   return gulp.src(options.src)
     .pipe(postcss(processors))
     .pipe(gulpif(options.banner, header(banner, { pkg : pkg } )))
@@ -72,65 +70,90 @@ var buildTask = function(options) {
     .pipe(gulpif(options.minify, gulp.dest(options.dest)))
 }
 
-var copyHTML = function(options) {
+// html build options
+var html = function (options) {
   return gulp.src(options.src)
     .pipe(gulp.dest(options.dest))
 }
 
-var copyImages = function(options) {
+// image options TODO: optimize images
+var images = function (options) {
   return gulp.src(options.src)
     .pipe(gulp.dest(options.dest))
 }
 
-gulp.task('dev', function() {
-  buildTask({
-    src: paths.css.src,
+/* javascript build options TODO: minimize js
+var js = function (options) {
+  return gulp.src(options.src)
+    .pipe(gulp.dest(options.dest))
+}
+*/
+
+ gulp.task('dev', function() {
+  css({
+    src: paths.src.css,
     banner: false,
     minify: false,
-    dest: paths.css.dev,
+    dest: paths.dev.css,
   })
-  copyHTML({
-    src: paths.html.src,
-    dest: paths.html.dev
+  html({
+    src: paths.src.html,
+    dest: paths.dev.html,
   })
-  copyImages({
-    src: paths.images.src,
-    dest: paths.images.dev
+  /*
+  js({
+    src: paths.src.js,
+    dest: paths.dev.js,
+  })
+  */
+  images({
+    src: paths.src.images,
+    dest: paths.dev.images,
   })
 })
 
 function setLiveReload() {
-  // if we have a RELOAD thing set, use that
-  // otherwise default to false
-  if (process.env.RELOAD) {
+  // if we have a RELOAD env variable set, use that otherwise default to false
     return process.env.RELOAD === "true" ? true : false
-  } else {
-    return false
-  }
 }
 
 var useLiveReload = setLiveReload()
 
 gulp.task("serve", function () {
-  gConn.server({
+  connect.server({
     root: paths.serve.root,
     port: process.env.PORT || 3000,
     livereload: useLiveReload
   })
 })
 
+// watches the dev dir for changes and hits the reload button
 gulp.task('reload', function () {
-  gulp.src(paths.serve.root) // this just watches the dev dir for changes and hits the reload button
-    .pipe(gConn.reload())
+  gulp.src(paths.serve.root)
+    .pipe(connect.reload())
 })
 
-
+// watch src files
 gulp.task('watch', function() {
   gulp.watch(paths.watch, ['dev'])
+  if (useLiveReload)
+    gulp.watch(paths.watch, ['reload'])
 })
 
+// create a new release
+function release(options) {
+  return gulp.src('./package.json')
+    .pipe(bump({type: options}))
+    .pipe(gulp.dest('./'))
+}
+
+gulp.task('patch', function() { return release('patch'); })
+gulp.task('feature', function() { return release('minor'); })
+gulp.task('release', function() { return release('major'); })
+
+// create production ready files
 gulp.task('prod', function() {
-  buildTask({
+  css({
     src: paths.css.src,
     banner: true,
     minify: true,
@@ -146,8 +169,9 @@ gulp.task('prod', function() {
       roundingPrecision: 10,
       shorthandCompacting: false
     },
-    dest: paths.css.dist,
+    dest: paths.dist.css
   })
 })
 
+// create a dev build, watch it for changes and serve it on localhost:3000
 gulp.task('default', ['dev', 'watch', 'serve'])
